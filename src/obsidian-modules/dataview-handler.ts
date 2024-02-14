ae2c1bcb4ed63f6662b40ce924bdc3b416abd87d
Joshua Pham <joshua.pham@gmail.com>
2024-02-14 09:22:10 -0800

    fix type issue

--- src/obsidian-modules/dataview-handler.ts
+++ src/obsidian-modules/dataview-handler.ts
@@ -1,10 +1,17 @@
 import { App, Notice } from 'obsidian'
 import { DataviewApi, getAPI as getNativeDataviewAPI } from 'obsidian-dataview'
 
-const canUseDataview = (app: App): boolean => {
-	return !!app.plugins.getPlugin('dataview')
+// TODO this is temporary until Obsidian typedef is updated
+interface AppWithPlugins extends App {
+	plugins: {
+		getPlugin(pluginId: string): any
+	}
 }
 
+const canUseDataview = (app: AppWithPlugins): boolean => {
+	const appWithPlugins = app as AppWithPlugins
+	return !!appWithPlugins.plugins.getPlugin('dataview')
+}
 const getAPI = (app: App): DataviewApi => {
 	if (!canUseDataview(app)) {
 		new Notice('Dataview plugin is not installed')
