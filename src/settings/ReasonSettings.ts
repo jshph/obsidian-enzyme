9bb8f2c12861759143b79abea6f55c008457e366
Joshua Pham <joshua.pham@gmail.com>
2024-03-09 20:48:16 -0500

    update default settings with LM studio and 0125 openai models

--- src/settings/ReasonSettings.ts
+++ src/settings/ReasonSettings.ts
@@ -15,7 +15,7 @@ export const DEFAULT_SETTINGS: ReasonSettings = {
 	models: [
 		{
 			label: 'GPT-3.5 Turbo',
-			model: 'gpt-3.5-turbo-1106',
+			model: 'gpt-3.5-turbo-0125',
 			apiKey: null,
 			baseURL: null
 		},
@@ -24,6 +24,12 @@ export const DEFAULT_SETTINGS: ReasonSettings = {
 			model: 'gpt-4-0125-preview',
 			apiKey: null,
 			baseURL: null
+		},
+		{
+			label: 'LM Studio',
+			model: '',
+			baseURL: 'http://localhost:1234/v1',
+			apiKey: ''
 		}
 	],
 	selectedModel: 'GPT-3.5 Turbo',
